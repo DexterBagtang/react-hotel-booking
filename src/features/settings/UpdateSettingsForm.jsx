33f57e1157d3ed9f21bd0dc152368317096a23e3
Dexter.Bagtang <dexterbagtang@gmail.com>
2025-02-27 10:47:56 +0800

    feat: Add settings update functionality
    This commit introduces a new feature to update settings.  It adds a new hook `useEditSettings` to handle API calls for updating settings and disables input fields while updating.  The `UpdateSettingsForm` now uses this hook to update settings and provides visual feedback during the update process.  No breaking changes are introduced.

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import {useSettings} from "./useSettings.js";
 import Spinner from "../../ui/Spinner.jsx";
+import {useEditSettings} from "./useEditSettings.js";
 
 function UpdateSettingsForm() {
     const {
@@ -16,6 +17,16 @@ function UpdateSettingsForm() {
             } = {}
     } = useSettings();
 
+    const {isUpdating , updateSetting} = useEditSettings();
+
+    function handleUpdate(e,field){
+        const {value}   = e.target;
+        if(!value) return;
+
+        updateSetting({[field]:value})
+
+    }
+
     if(isLoading) return <Spinner />;
 
     return (
@@ -23,21 +34,33 @@ function UpdateSettingsForm() {
             <FormRow label='Minimum nights/booking'>
                 <Input type='number' id='min-nights'
                        defaultValue={minBookingLength}
+                       disabled={isUpdating}
+                       onBlur={e=>
+                           handleUpdate(e,'minBookingLength')}
                 />
             </FormRow>
             <FormRow label='Maximum nights/booking'>
                 <Input type='number' id='max-nights'
                        defaultValue={maxBookingLength}
+                       disabled={isUpdating}
+                       onBlur={e=>
+                           handleUpdate(e,'maxBookingLength')}
                 />
             </FormRow>
             <FormRow label='Maximum guests/booking'>
                 <Input type='number' id='max-guests'
                        defaultValue={maxGuestPerBooking}
+                       disabled={isUpdating}
+                       onBlur={e=>
+                           handleUpdate(e,'maxGuestPerBooking')}
                 />
             </FormRow>
             <FormRow label='Breakfast price'>
                 <Input type='number' id='breakfast-price'
                        defaultValue={breakfastPrice}
+                       disabled={isUpdating}
+                       onBlur={e=>
+                           handleUpdate(e,'breakfastPrice')}
                 />
             </FormRow>
         </Form>
