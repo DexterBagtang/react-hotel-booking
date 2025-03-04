6984ffd7913c4dc0ee7e50bbd012e33fa43ef272
Dexter.Bagtang <dexterbagtang@gmail.com>
2025-03-04 11:11:58 +0800

    feat(bookings): Add booking filtering and operations
    Add filtering options to the bookings page via URL parameters.  Implement a new component to handle booking operations. Update the Bookings page to include the operations component.
    
    Refactor: Improved data fetching and UI for Bookings page.

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,16 +1,21 @@
 import {useQuery} from "@tanstack/react-query";
 import {getBookings} from "../../services/apiBookings.js";
+import {useSearchParams} from "react-router-dom";
 
 export function useBookings() {
-    //Fetches the cabins in the database using React Query useQuery hook
+    const [searchParams] = useSearchParams();
+    const filterValue = searchParams.get('status')
+    const filter = !filterValue || filterValue === 'all' ? null : {field:'status',value:filterValue};
+
+    //Fetches the bookings in the database using React Query useQuery hook
     const {
         isLoading,
         data:bookings,
         error
     }
         = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings,
+        queryKey: ["bookings",filter],
+        queryFn:()=> getBookings({filter}),
     })
 
     return {isLoading, bookings,error};
