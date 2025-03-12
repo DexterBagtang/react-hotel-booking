4721110ee1994a1a09d760ad82352c754332baf6
Dexter.Bagtang <dexterbagtang@gmail.com>
2025-03-12 09:52:07 +0800

    fix(check-in-out): improve navigation after check-in/out
    Update navigation after successful check-in/check-out to use `navigate(-1)` instead of redirecting to '/bookings'. This provides a better user experience by returning users to the previous screen.
    
    - Replaced `/bookings` navigation with `navigate(-1)` in `useCheckin` and `useCheckout`.
    - Changed isLoading to isPending in useCheckout hook for better clarity.
    - Improved user experience by returning to the previous screen.

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -6,14 +6,14 @@ import {useNavigate} from "react-router-dom";
 export function useCheckout() {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
-    const {mutate: checkout, isLoading:isCheckingOut} = useMutation({
+    const {mutate: checkout, isPending:isCheckingOut} = useMutation({
         mutationFn: (bookingId) => updateBooking(bookingId,{
             status: 'checked-out',
         }),
         onSuccess: (data) => {
             toast.success(`Booking #${data.id} successfully checked out.`);
             queryClient.invalidateQueries({active:true});
-            navigate('/bookings')
+            // navigate(-1)
         },
 
         onError: () => {
