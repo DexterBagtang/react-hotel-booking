91d17f6a6f7c792e36fef0baccd06ed46fadd6d6
Dexter.Bagtang <dexterbagtang@gmail.com>
2025-03-12 09:52:19 +0800

    feat(error-handling): Implement error boundary
    Improve application stability by adding a global error boundary.
    
    • Added a custom ErrorFallback component to display user-friendly error messages.
    • Integrated react-error-boundary library.
    • Improved error handling in BookingTable and main components.
    
    BREAKING CHANGE: Updated main.jsx to use error boundary.  Consider updating affected components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
+import {ErrorBoundary} from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            fallback={<div>errror</div>}
+            onReset={()=> window.location.replace('/')}
+        >
+            <App/>
+        </ErrorBoundary>
+    </StrictMode>,
 )
